a7d037061eb7ad4006b44d852e7641a5c12850bb
Sebastián <juansebasmorenoruiz@gmail.com>
2023-06-17 23:20:49 -0500

    adding vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+function addDeferAttribute(): Plugin {
+    return {
+        generateBundle(_, bundle) {
+            for (const fileName in bundle) {
+                if (bundle[fileName].type === 'chunk') {
+                    bundle[fileName].isEntry = true;
+                }
+            }
+        },
+        transformIndexHtml(html) {
+            return html.replace('<script type="module" crossorigin ', '<script defer ');
+        },
+    };
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "./",
+    build: {
+        rollupOptions: {
+            output: {
+                format: "iife"
+            }
+        }
+    },
+    plugins: [react(), addDeferAttribute()],
 })
