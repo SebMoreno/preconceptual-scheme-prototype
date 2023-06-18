42b3bb3098992b88d9370a72327d303bf0bd9084
Sebastián <juansebasmorenoruiz@gmail.com>
2023-06-17 23:32:31 -0500

    reformatting vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 
-function addDeferAttribute(): Plugin {
+function addDeferAttribute(): PluginOption {
     return {
-        generateBundle(_, bundle) {
-            for (const fileName in bundle) {
-                if (bundle[fileName].type === 'chunk') {
-                    bundle[fileName].isEntry = true;
-                }
-            }
-        },
+        name: "addDeferAttribute",
         transformIndexHtml(html) {
-            return html.replace('<script type="module" crossorigin ', '<script defer ');
-        },
+            return html.replace('<script type="module" crossorigin', '<script defer');
+        }
     };
 }
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
