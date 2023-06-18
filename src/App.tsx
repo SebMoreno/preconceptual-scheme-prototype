6f6321d44f7be3680be2f832304ee4c03026d498
Sebastián <juansebasmorenoruiz@gmail.com>
2023-06-17 22:01:04 -0500

    adding dialog and buttons for example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import './App.css'
-import { PreconceptualScheme } from "./components/PreconceptualScheme.tsx";
+import { TableDataGrid } from "./components/TableDataGrid.tsx";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from "react";
+import { Button, Dialog, DialogContent } from "@mui/material";
+
+const darkTheme = createTheme({palette: {mode: 'dark'}});
 
 export const App = () => {
+    const [tableName, setTableName] = useState("ProjectManagementSoftwares")
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenTable = (tableName: string) => {
+        setTableName(tableName)
+        setIsOpen(true)
+    }
     return (
-        <PreconceptualScheme/>
+        <ThemeProvider theme={darkTheme}>
+            {/*<PreconceptualScheme onOpenTable={handleOpenTable}/>*/}
+            <Button onClick={() => handleOpenTable("ProjectManagementSoftwares")}>
+                Open modal for ProjectManagementSoftwares
+            </Button>
+            <Button onClick={() => handleOpenTable("SoftwareDevelopmentProjects")}>
+                Open modal for ProjectManagementSoftwares
+            </Button>
+            <Dialog
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+            >
+                <DialogContent sx={{px: 5, py: 3}}>
+                    <TableDataGrid tableName={tableName}/>
+                </DialogContent>
+            </Dialog>
+        </ThemeProvider>
     )
 }
