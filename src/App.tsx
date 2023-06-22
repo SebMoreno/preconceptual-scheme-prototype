445c152e049f804578b0c2b060f4a81a61936310
Sebastián <juansebasmorenoruiz@gmail.com>
2023-06-21 20:45:06 -0500

    adding error handling with alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import DownloadIcon from "@mui/icons-material/Download";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import LockResetIcon from "@mui/icons-material/LockReset";
-import { Dialog, DialogContent, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
+import {
+    Alert,
+    AlertColor,
+    Dialog,
+    DialogContent,
+    Snackbar,
+    SpeedDial,
+    SpeedDialAction,
+    SpeedDialIcon
+} from "@mui/material";
 import React, { useRef, useState } from "react";
 import { PreconceptualScheme } from "./components/PreconceptualScheme.tsx";
 import { TableDataGrid, TableDataGridProps } from "./components/TableDataGrid.tsx";
@@ -11,50 +20,77 @@ import { downloadJsonFile } from "./services/downloadJsonFile.ts";
 
 export const App = () => {
     const inputFileRef = useRef<HTMLInputElement>(null);
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [alertInfo, setAlertInfo] = useState<{ isShown?: boolean, message?: string, severity?: AlertColor }>({});
     const [tableDataGridProps, setTableDataGridProps] = useState<TableDataGridProps>({
         tableName: "",
         subTitle: ""
     });
     const handleOpenTable = (newTableDataGridProps: TableDataGridProps) => {
-        setIsOpen(true);
+        setIsOpenModal(true);
         setTableDataGridProps(newTableDataGridProps);
     };
     const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            await db.loadDataFromObject(JSON.parse(await file.text()));
-            event.target.value = '';
-            alert("Data loaded successfully.");
+        try {
+            const file = event.target.files?.[0];
+            if (file) {
+                await db.loadDataFromObject(JSON.parse(await file.text()));
+                event.target.value = '';
+                setAlertInfo({
+                    isShown: true,
+                    message: "Data loaded successfully.",
+                    severity: "success"
+                });
+            }
+        } catch (e) {
+            console.error(e);
+            setAlertInfo({
+                isShown: true,
+                message: "Error loading data. Please reload the page and try again.",
+                severity: "error"
+            });
         }
     };
     const measureTechDebt = async () => {
-        const violations = await db.Violation.toArray();
-        const elementosAgrupados = violations.reduce((groups, v) => {
-            if (groups[v.codeId]) {
-                groups[v.codeId].push(v);
-            } else {
-                groups[v.codeId] = [v];
-            }
-            return groups;
-        }, {} as { [group: number]: Violation[] });
-        for (const [codeId, violations] of Object.entries(elementosAgrupados)) {
-            const code = await db.Code.get(parseInt(codeId));
-            if (!code) {
-                continue;
-            }
-            code.technicalDebt = (await Promise.all(violations.map(async v => {
-                const qualityRule = await db.QualityRule.get(v.qualityRuleId);
-                if (qualityRule) {
-                    qualityRule.principal = typeof qualityRule.principal === "string" ? parseFloat(qualityRule.principal) : qualityRule.principal;
-                    qualityRule.interest = typeof qualityRule.interest === "string" ? parseFloat(qualityRule.interest) : qualityRule.interest;
-                    return qualityRule.principal + qualityRule.interest;
+        try {
+            const violations = await db.Violation.toArray();
+            const elementosAgrupados = violations.reduce((groups, v) => {
+                if (groups[v.codeId]) {
+                    groups[v.codeId].push(v);
+                } else {
+                    groups[v.codeId] = [v];
                 }
-                return 0;
-            }))).reduce((a, b) => a + b);
-            await db.Code.put(code);
+                return groups;
+            }, {} as { [group: number]: Violation[] });
+            for (const [codeId, violations] of Object.entries(elementosAgrupados)) {
+                const code = await db.Code.get(parseInt(codeId));
+                if (!code) {
+                    continue;
+                }
+                code.technicalDebt = (await Promise.all(violations.map(async v => {
+                    const qualityRule = await db.QualityRule.get(v.qualityRuleId);
+                    if (qualityRule) {
+                        qualityRule.principal = typeof qualityRule.principal === "string" ? parseFloat(qualityRule.principal) : qualityRule.principal;
+                        qualityRule.interest = typeof qualityRule.interest === "string" ? parseFloat(qualityRule.interest) : qualityRule.interest;
+                        return qualityRule.principal + qualityRule.interest;
+                    }
+                    return 0;
+                }))).reduce((a, b) => a + b);
+                await db.Code.put(code);
+            }
+            setAlertInfo({
+                isShown: true,
+                message: "Technical debt measurement successfully completed. Please review the Codes table.",
+                severity: "success"
+            });
+        } catch (e) {
+            console.error(e);
+            setAlertInfo({
+                isShown: true,
+                message: "Error measuring technical debt. Please reload the page and try again.",
+                severity: "error"
+            });
         }
-        alert("Technical debt measurement successfully completed, please review the Codes table.");
     };
     return (
         <>
@@ -86,8 +122,8 @@ export const App = () => {
             </SpeedDial>
             <Dialog
                 maxWidth="lg"
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
+                open={isOpenModal}
+                onClose={() => setIsOpenModal(false)}
             >
                 <DialogContent sx={{px: 5, py: 3}}>
                     <TableDataGrid
@@ -99,6 +135,18 @@ export const App = () => {
                     />
                 </DialogContent>
             </Dialog>
+            <Snackbar
+                anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+                open={alertInfo.isShown}
+                autoHideDuration={5000}
+                onClose={() => setAlertInfo({isShown: false})}>
+                <Alert
+                    onClose={() => setAlertInfo({isShown: false})}
+                    severity={alertInfo.severity}
+                    sx={{width: '100%'}}>
+                    {alertInfo.message}
+                </Alert>
+            </Snackbar>
         </>
     );
 };
